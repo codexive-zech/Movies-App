1d13c2eb5956bc548d79bcb4c0336c95dee8d186
Zechariah hounwanou <zechariahhounwanou@gmail.com>
2023-04-14 00:06:24 +0100

    Added the About section page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <>
+      <div className=" w-[90vw] mx-auto max-w-6xl space-y-12">
+        <h2 className=" text-2xl lg:text-3xl text-center mt-4 text-amber-600 font-semibold capitalize">
+          About Chic Movies
+        </h2>
+        <p>
+          Welcome to Chic Movie database website! We are a team of professional
+          movies lovers who have come together to create a platform for all your
+          movies related needs. Our website is designed to provide you with a
+          comprehensive database of movies from all around the world, along with
+          the latest movies, reviews and trailers. Our Movies website is always
+          update with new movies, ensuring that you have access to the latest
+          and greatest in the world of cinema. You can search for movies by
+          title making it easy to find the perfect movie for the occasion
+        </p>
+        <p>
+          Thank you for visiting our website and we hope you enjoy your time
+          browsing through our database. If you have any suggestion or feedback,
+          please do well to contact us. We are always looking for ways to
+          improve and enhance the user experience of our website. Thank You.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default AboutPage;
